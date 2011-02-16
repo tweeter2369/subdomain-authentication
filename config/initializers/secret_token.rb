c89f307cc8136bd7a9e2a55725d590746c1912b7
tweeter2369 <doylepaul23@gmail.com>
2011-02-15 18:32:10 -0800

    first load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubdomainAuthentication::Application.config.secret_token = '551d3e46f42a48ac681f25962beefe189feab18b4e0b3ed74ef0567668243f13c34c4896cb4e0fe25a92c8304c8d7e8948cfc43bea7ec2d2a6d4cda65ed0ec08'
